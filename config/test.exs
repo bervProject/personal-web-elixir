2c80babe87495a9607c9f81c7c9fe857ae795fdf
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-11-07 23:28:25 +0700

    unified pass

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ import Config
 # Run `mix help test` for more information.
 config :personal_web, PersonalWeb.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "devsecretpassword",
   database: "personal_web_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox,
