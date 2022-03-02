8f283fb662bb12693c1f13f6d4de5994e2eb7c69
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-03-02 13:28:52 +0700

    update mix.exs and mix.lock

--- mix.exs
+++ mix.exs
@@ -42,26 +42,26 @@ defmodule PersonalWeb.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.2"},
-      {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 3.0"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.16.0"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.5"},
-      {:esbuild, "~> 0.2", runtime: Mix.env() == :dev},
-      {:swoosh, "~> 1.3"},
-      {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.18"},
-      {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
       {:absinthe_plug, "~> 1.5"},
-      {:excoveralls, "~> 0.10", only: :test},
+      {:ecto_sql, "~> 3.7"},
+      {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
+      {:excoveralls, "~> 0.14", only: :test},
+      {:floki, ">= 0.32.0", only: :test},
+      {:gettext, "~> 0.19"},
+      {:jason, "~> 1.3"},
+      {:mimic, "~> 1.5", only: :test},
+      {:phoenix, "~> 1.6"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:phoenix_html, "~> 3.2"},
+      {:phoenix_live_dashboard, "~> 0.6"},
+      {:phoenix_live_reload, "~> 1.3", only: :dev},
+      {:phoenix_live_view, "~> 0.17"},
+      {:plug_cowboy, "~> 2.5"},
+      {:postgrex, ">= 0.0.0"},
       {:sobelow, "~> 0.11", only: :dev},
-      {:mimic, "~> 1.5", only: :test}
+      {:swoosh, "~> 1.6"},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 1.0"}
     ]
   end
 
